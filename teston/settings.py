b359867ec0075203f2b8dd9e242a1eda028634b2
Vitaliy Franchook <vfranchook@gmail.com>
2015-05-27 15:14:19 +0300

    Another sueless try

--- teston/settings.py
+++ teston/settings.py
@@ -101,6 +101,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+"""
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static' # can be problem
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
@@ -110,5 +120,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'teston/static'),
     
 )
-
+"""
 
